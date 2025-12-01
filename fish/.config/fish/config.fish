5b7c088707accb7edb8010d2ea01029b085cd47f
Deepta <deeptoadhikary@gmail.com>
2025-12-01 19:21:08 +0600

    fish config updated

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,18 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# overwrite greeting
-# potentially disabling fastfetch
 function fish_greeting
-    starship init fish | source
-    zoxide init --cmd cd fish | source
+
 end
 
+
+set -g fish_key_bindings fish_vi_key_bindings
+set fish_cursor_default block blink
+
+# fzf shell integration:
+fzf --fish | source
+
+# starship shell integratin:
+starship init fish | source
+
+# zoxide shell integration:
+zoxide init --cmd cd fish | source
